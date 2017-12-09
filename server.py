1af7298a5e80ebbc017b0a118c2b82047ec8565c
mayank-git-hub <32998770+mayank-git-hub@users.noreply.github.com>
2017-12-09 18:33:41 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 #Run this first on the computer where you want to recieve the video
 
-
 from socket import *
 import cv2
 import numpy as np
@@ -28,16 +27,20 @@ UDPSock_send = socket(AF_INET, SOCK_DGRAM)
 i_ = np.uint8(1).tostring()
 
 while 1:
+    
     frame = np.zeros(921600).astype(np.uint8)
 
     for i in range(no_packet):
+        
         data,addr = UDPSock.recvfrom(buf)
         frame[i*frame.shape[0]//no_packet:(i+1)*frame.shape[0]//no_packet] = np.fromstring(data, dtype = np.uint8)
 
     cv2.imshow('frame', np.reshape(frame, (480, 640, 3)))
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+        
+    # For acknowledgement, otherwise UDP will keep on sending data and that will overlap the buffer
     UDPSock.sendto(i_, addr_send)
 
 UDPSock.close()
