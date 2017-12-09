51051313f7bdc5645ad5531a3bb2e0af4c1c77e9
mayank-git-hub <32998770+mayank-git-hub@users.noreply.github.com>
2017-12-09 18:29:43 +0530

    Update client.py

--- client.py
+++ client.py
@@ -2,14 +2,15 @@ from socket import *
 import cv2
 import numpy as np
 
-host = "192.168.0.103"
+host = "192.168.0.103"	# Here goes the IP address of the computer you want to send the video to
+
 port = 13000
 addr = (host, port)
 UDPSock = socket(AF_INET, SOCK_DGRAM)
 no_packet = 50
 
 
-host_recv = "192.168.0.101"
+host_recv = "192.168.0.101"	# Here goes the IP address of the computer having the video
 port_recv = 13001
 buf = 50
 addr_recv = (host_recv, port_recv)
