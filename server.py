3424a6eb015231224094afed1dd371ed2a014067
Mayank Kumar Singh <mayank25031998@gmail.com>
2019-09-13 21:56:46 +0530

    server_compressed

--- server.py
+++ server.py
@@ -4,29 +4,39 @@ from socket import *
 import cv2
 import numpy as np
 import config
+from PIL import Image
+import io
 
 buf = 184650
 addr = (config.receiver_ip, config.receiver_port)
 
 UDPSock = socket(AF_INET, SOCK_DGRAM)
 UDPSock.bind(addr)
-no_packet = 50
 
 addr_send = (config.sender_ip, config.sender_port)
 UDPSock_send = socket(AF_INET, SOCK_DGRAM)
 
 i_ = np.uint8(1).tostring()
 
+
 while True:
-    
+
+    data, addr = UDPSock.recvfrom(buf)
+
+    length = int(data)
+    UDPSock.sendto(i_, addr_send)
+
+    current_image = b''
     frame = np.zeros(921600).astype(np.uint8)
 
-    for i in range(no_packet):
+    while len(current_image) != length:
         
         data, addr = UDPSock.recvfrom(buf)
-        frame[i*frame.shape[0]//no_packet:(i+1)*frame.shape[0]//no_packet] = np.fromstring(data, dtype=np.uint8)
+        current_image += data
 
-    cv2.imshow('frame', np.reshape(frame, (480, 640, 3)))
+    frame = np.array(Image.open(io.BytesIO(current_image)))
+
+    cv2.imshow('frame', frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
